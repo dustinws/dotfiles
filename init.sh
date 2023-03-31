cd93d22fd19ea483c3252c573fe121c436d0394d
Dustin Stiles <dustin.w.stiles2.mil@mail.mil>
2023-03-31 17:58:42 -0400

    reorganize dotfiles

--- init.sh
+++ init.sh
@@ -3,16 +3,11 @@
 rm ~/.zshrc
 rm ~/.tmux.conf
 
-rm -rf ~/.config/kitty
 rm -rf ~/.config/nvim
 
-echo "source ~/dotfiles/.zshrc" >> ~/.zshrc
-echo "source ~/dotfiles/.tmux.conf" >> ~/.tmux.conf
+echo "source ~/Dotfiles/zsh/zshrc" >> ~/.zshrc
+echo "source ~/Dotfiles/tmux/tmux.conf" >> ~/.tmux.conf
 
 mkdir -p ~/.config/nvim
-ln -s ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Dotfiles/nvim/init.vim ~/.config/nvim/init.vim
 nvim +'PlugInstall --sync' +qa
-
-mkdir -p ~/.config/kitty
-ln -s ~/dotfiles/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/dotfiles/.config/kitty/nord.conf ~/.config/kitty/nord.conf
